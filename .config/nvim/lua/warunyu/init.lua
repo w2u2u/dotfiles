f5e0f1dcf0a3a0e49de67cb3e19834a5000db8cc
Warunyu Jaimuang <warunyu.pon@gmail.com>
2024-02-28 13:42:39 +0700

    fix(neovim): clipboard config

--- .config/nvim/lua/warunyu/init.lua
+++ .config/nvim/lua/warunyu/init.lua
@@ -3,6 +3,7 @@ vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 5
+vim.opt.clipboard = "unnamedplus"
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
